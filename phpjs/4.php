6f414320e47c4828955900407c1a6c4438b77f91
Fire <fire@gmail.com>
2017-06-12 10:56:22 +0900

    6/12

---
+++ phpjs/4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+	<h1>javascript</h1>
+	<script charset="utf-8">
+		name = "egoing";
+		if(1===1)
+			document.write("안녕하세요" + name);
+	</script>
+	<?php
+		$name = "egoing";
+		echo "안녕하세요".$name;
+	?>
+<body>
+</body>
+</html>
