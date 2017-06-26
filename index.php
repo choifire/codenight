6409b6c5ac203fa97d5115cbb444f7e7e4b7fb4d
Fire <fire@gmail.com>
2017-06-26 16:29:08 +0900

    6/26

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         <ol>
         <?php 
           while($row = mysqli_fetch_assoc($result)){
-            echo '<li><a href="http://localhost:8080/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
+            echo '<li><a href="http://localhost:8080/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
           }
         ?>
         </ol>
@@ -32,12 +32,13 @@
   <article>
     <?php
       if(empty($_GET['id']) === false){
-        $sql = 'SELECT * FROM topic WHERE id='.$_GET['id'];
+        $sql = 'SELECT topic.id, title, name, description FROM topic LEFT JOIN user ON topic.author = user.id WHERE topic.id='.$_GET['id'];
         $result = mysqli_query($conn, $sql);
         $row = mysqli_fetch_assoc($result);
 
-        echo '<h2>'.$row['title'].'</h2>';
-        echo $row['description'];
+        echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
+        echo '<p>'.htmlspecialchars($row['name']).'</p>';
+        echo strip_tags($row['description'], "<a><h1><h2><h3><h4><h5><ul><ol><li>");
       }
     ?>
   </article>
